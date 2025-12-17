aff1abe2eac6bab8a62121c6a490b81711ea129c
mike <mike@MacBook-Air.local>
2025-12-17 16:07:04 -0500

    ci: make stateCityMap.ci.js fully standalone

--- data/stateCityMap.ci.js
+++ data/stateCityMap.ci.js
@@ -1,4 +1,21 @@
 // data/stateCityMap.ci.js
-import stateCityMap from "../app/lib/stateCityMap.js";
+export default {
+  texas: {
+    stateName: "Texas",
+    stateLoanSlug: "texas",
+    cities: [
+      { slug: "houston", name: "Houston" },
+      { slug: "dallas", name: "Dallas" },
+      { slug: "austin", name: "Austin" }
+    ]
+  },
 
-export default stateCityMap;
+  california: {
+    stateName: "California",
+    stateLoanSlug: "california",
+    cities: [
+      { slug: "los-angeles", name: "Los Angeles" },
+      { slug: "san-diego", name: "San Diego" }
+    ]
+  }
+};
